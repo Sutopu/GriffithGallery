5bc85ea23fbc607110a725cc5e38fa60904c27e4
TravisTheCoolestGuyInTheUniverse <hydra5732@yahoo.com>
2020-07-30 13:24:47 -0400

    a dynamic amount of images can now be displayed on the webpage (in a super ugly fashion at the moment)

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, url_for
+import glob
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    imagesSrc = glob.glob("static/pictures/*")
+    return render_template("index.html", imagesSrc = imagesSrc)
 
 if __name__ == "__main__":
     app.run(debug=True)
