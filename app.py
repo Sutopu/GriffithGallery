42aed87ad7da546a56391b6af27856d22b4584b3
TravisTheCoolestGuyInTheUniverse <hydra5732@yahoo.com>
2020-08-04 03:05:27 -0400

    added some aditional documentation and also changed the behavior of the carousel so that it does not autoplay.

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #file locations for all pictures of Griffith
     imagesSrc = glob.glob("static/pictures/*")
+    #render the template using pictures at locations above
     return render_template("index.html", imagesSrc = imagesSrc)
 
 
